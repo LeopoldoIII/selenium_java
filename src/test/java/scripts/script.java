262ddc2e11832ad851dbe2d8c1fa6ae7ca4c00d1
polo <nathan.rx@gmail.com>
2019-02-07 16:08:57 -0600

    -First Script

---
+++ src/test/java/scripts/script.java
@@ -0,0 +1,44 @@
+package scripts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class script {
+
+    @Test
+    public void Chorme(){
+        //webdriver object driver
+
+        WebDriver driver;
+
+        //Variables
+
+        String baseURL = "http://newtours.demoaut.com/";
+        String actualResult;
+        String expectedReuslt = "Welcome: Mercury TourS";
+
+        //Driver Path
+
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\driver\\chromedriver.exe");
+
+
+        //Test
+
+        driver = new ChromeDriver();
+
+
+        //Interation
+        driver.get(baseURL);
+
+        actualResult = driver.getTitle();
+
+        System.out.println();
+        System.out.println(actualResult.contentEquals(expectedReuslt)?"Prueba pasada! " + actualResult : "Prueba Fallada " + actualResult);
+
+        driver.close();
+
+
+
+    }
+}
